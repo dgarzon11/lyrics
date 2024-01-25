9c79f68e7f6cbfe0936ecb7ae16742f4c3c2ceb3
Diego <dgarzon11@gmail.com>
2024-01-25 14:54:25 -0500

    Update lyrics.py

--- lyrics.py
+++ lyrics.py
@@ -1,38 +1,36 @@
 import streamlit as st
 from lyricsgenius import Genius
 
-# Configura tu token de acceso de Genius aquí
-genius_token = "lxQLnHm5VWku3CCkrdnpvGPNH778y4jPuGMQL7zBZr-86CjbT8xIi9vcM6XjA37n"
+# Configure your Genius access token here
+genius_token = "YOUR_GENIUS_TOKEN"
 genius = Genius(genius_token)
 
 st.title('Song Lyrics Finder')
 
-# Entradas del usuario para el artista y la canción
+# User inputs for artist and song name
 artist_name = st.text_input('Artist Name')
 song_name = st.text_input('Song Name')
 
-# Función para buscar la canción en Genius y obtener la letra
+# Function to search for the song on Genius and retrieve lyrics
 import re
 
 def get_lyrics(song, artist):
     song = genius.search_song(title=song, artist=artist)
     if song:
         lyrics = song.lyrics
-        # Elimina el texto antes de "Lyrics"
+        # Remove text before "Lyrics"
         start_of_lyrics = lyrics.find('Lyrics')
         if start_of_lyrics != -1:
             lyrics = lyrics[start_of_lyrics + len('Lyrics'):]
 
-        # Elimina el texto como "23Embed" del final
+        # Remove text like "23Embed" at the end
         lyrics = re.sub(r'\d+Embed$', '', lyrics, flags=re.MULTILINE)
 
-        return lyrics.strip()  # Elimina espacios adicionales al principio y al final
+        return lyrics.strip()  # Remove extra spaces at the beginning and end
     else:
         return "Lyrics not found."
 
-
-
-# Mostrar la letra si el usuario ha ingresado ambos, el artista y la canción
+# Display lyrics if both artist and song are entered by the user
 if artist_name and song_name:
     lyrics = get_lyrics(song_name, artist_name)
     st.text_area('Lyrics', lyrics, height=300)
